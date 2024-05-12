8141744777b5b83e77fcf7fadfeb153867588745
NikolaiSobi <nikolaisobioch@gmail.com>
2024-05-12 00:06:36 -0600

    first commit

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -16,7 +16,7 @@ it("should return a result with 2 decimal places", function() {
   const values = {
     amount: 1212,
     years: 5,
-    rate: 7
+    rate: 7 
   }
   expect(calculateMonthlyPayment(values)).toEqual("24.00")
 });
